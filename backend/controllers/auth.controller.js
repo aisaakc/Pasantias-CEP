df3a28fe9b83036376e97380aa964bd11f12a815
isaac cattoni <aysaakc19@gmail.com>
2025-04-08 13:06:25 -0400

    api de login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import pool from '../db.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = async (req, res) => {
   const {
@@ -10,7 +11,7 @@ export const registerUser = async (req, res) => {
     telefono,
     password,
     tipoParticipante,
-    genero, // Nuevo campo
+    genero, 
   } = req.body;
 
   try {
@@ -51,6 +52,42 @@ export const registerUser = async (req, res) => {
     res.status(201).json({ message: 'Usuario registrado exitosamente' });
   } catch (err) {
     console.error('Error en el registro:', err.message);
+    res.status(500).json({ message: 'Error en el servidor' });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { correo, password } = req.body;
+
+  try {
+    if (!correo || !password) {
+      return res.status(400).json({ message: 'Correo y contraseña son requeridos' });
+    }
+
+    // Buscar al usuario por correo
+    const user = await pool.query('SELECT * FROM usuario WHERE correo = $1', [correo]);
+
+    if (user.rows.length === 0) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    // Comparar la contraseña
+    const isPasswordValid = await bcrypt.compare(password, user.rows[0].contraseña);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Contraseña incorrecta' });
+    }
+
+    // Crear el token JWT
+    const token = jwt.sign(
+      { userId: user.rows[0].id, nombre: user.rows[0].nombre, rol: user.rows[0].rol }, // payload
+      process.env.JWT_SECRET, // clave secreta
+      { expiresIn: '1h' } // tiempo de expiración del token
+    );
+
+    res.status(200).json({ message: 'Inicio de sesión exitoso', token });
+  } catch (err) {
+    console.error('Error en el login:', err.message);
     res.status(500).json({ message: 'Error en el servidor' });
   }
 };
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,12 @@
-// backend/routes/authRoutes.js
 import express from 'express';
-import { registerUser } from '../controllers/auth.controller.js';
+import { registerUser, loginUser } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
+// Ruta de registro
 router.post('/registro', registerUser);
+
+// Ruta de inicio de sesión
+router.post('/login', loginUser);
 
 export default router;
